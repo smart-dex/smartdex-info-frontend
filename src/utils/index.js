965d6965da2de5153ddae7d706edf1ab78705509
Sotatek-LocNgo <loc.ngo@sotatek.com>
2021-04-19 16:57:44 +0700

    fix 529

--- src/utils/index.js
+++ src/utils/index.js
@@ -40,13 +40,13 @@ export function getTimeframe(timeWindow) {
 export function getPoolLink(token0Address, token1Address = null, remove = false) {
   if (!token1Address) {
     return (
-      `https://exchange.pancakeswap.finance/#/` +
+      `${process.env.REACT_APP_EXCHANGE_URL}/#/` +
       (remove ? `remove` : `add`) +
       `/${token0Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token0Address}/${'ETH'}`
     )
   } else {
     return (
-      `https://exchange.pancakeswap.finance/#/` +
+      `${process.env.REACT_APP_EXCHANGE_URL}/#/` +
       (remove ? `remove` : `add`) +
       `/${token0Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token0Address}/${token1Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token1Address
       }`
@@ -56,9 +56,9 @@ export function getPoolLink(token0Address, token1Address = null, remove = false)
 
 export function getSwapLink(token0Address, token1Address = null) {
   if (!token1Address) {
-    return `https://exchange.pancakeswap.finance/#/swap?inputCurrency=${token0Address}`
+    return `${process.env.REACT_APP_EXCHANGE_URL}/#/swap?inputCurrency=${token0Address}`
   } else {
-    return `https://exchange.pancakeswap.finance/#/swap?inputCurrency=${token0Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token0Address
+    return `${process.env.REACT_APP_EXCHANGE_URL}/#/swap?inputCurrency=${token0Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token0Address
       }&outputCurrency=${token1Address === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' ? 'ETH' : token1Address}`
   }
 }
