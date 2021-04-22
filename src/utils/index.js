a76c4aaf6c874fedbb02095d0c0ba59e5e055aa6
Sotatek-LocNgo <loc.ngo@sotatek.com>
2021-04-22 17:03:30 +0700

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -339,7 +339,43 @@ export const toSignificant = (number, significantDigits) => {
   return updated.toFormat(updated.decimalPlaces(), { groupSeparator: '' })
 }
 
-export const formattedNum = (number, usd = false, check) => {
+export const formattedNum = (number, usd = false, acceptNegatives = false) => {
+  if (isNaN(number) || number === '' || number === undefined) {
+    return usd ? '$0' : 0
+  }
+  let num = parseFloat(number)
+
+  if (num > 500000000) {
+    return (usd ? '$' : '') + toK(num.toFixed(0), true)
+  }
+
+  if (num === 0) {
+    if (usd) {
+      return '$0'
+    }
+    return 0
+  }
+
+  if (num < 0.0001 && num > 0) {
+    return usd ? '< $0.0001' : '< 0.0001'
+  }
+
+  if (num > 1000) {
+    return usd ? formatDollarAmount(num, 0) : Number(parseFloat(num).toFixed(0)).toLocaleString()
+  }
+
+  if (usd) {
+    if (num < 0.1) {
+      return formatDollarAmount(num, 4)
+    } else {
+      return formatDollarAmount(num, 2)
+    }
+  }
+
+  return Number(parseFloat(num).toFixed(5)).toLocaleString()
+}
+
+export const formattedValue = (number, usd = false, check = false) => {
   if (isNaN(number) || number === '' || number === undefined) {
     return usd ? '$0' : 0
   }
