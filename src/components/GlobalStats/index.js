01a3edb3d76eacb712af8e697e7e94ba0a5ea7d2
vietnt <vietnt>
2021-05-04 10:30:28 +0700

    fix bug

--- src/components/GlobalStats/index.js
+++ src/components/GlobalStats/index.js
@@ -31,7 +31,7 @@ export default function GlobalStats() {
   const { oneDayVolumeUSD, oneDayTxns, pairCount } = useGlobalData()
   const [ethPrice] = useEthPrice()
   const formattedEthPrice = ethPrice ? formattedNum(ethPrice, true) : '-'
-  const oneDayFees = oneDayVolumeUSD ? formattedNum(oneDayVolumeUSD * 0.002, true) : ''
+  const oneDayFees = oneDayVolumeUSD ? formattedNum(oneDayVolumeUSD * 0.002, true) : 0
 
   return (
     <Header>
